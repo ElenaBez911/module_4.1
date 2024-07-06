cdf8f43d9e75c3ed8c137b41c0c961dee00b5383
ElenaBez911 <esheronovav@gmail.com>
2024-07-06 22:06:29 +0500

    Update fake_math.py

--- fake_math.py
+++ fake_math.py
@@ -3,9 +3,3 @@ def divide (first, second):
         return print("Ошибка")
     else:
         return first/second
-
-result1 = divide(69, 3)
-result2 = divide(3, 0)
-
-print(result1)
-print(result2)
